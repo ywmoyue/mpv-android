48f5d90e12ec552dde0afcc0ba3415cd462b4960
ywmoyue <28388300+ywmoyue@users.noreply.github.com>
2025-08-12 16:28:18 +0800

    ndk version

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -4,8 +4,8 @@
 # Make sure to keep v_ndk and v_ndk_n in sync, both are listed on the NDK download page
 
 v_sdk=11076708_latest
-v_ndk=r21e
-v_ndk_n=21.4.7075529
+v_ndk=r25c
+v_ndk_n=25.2.9519653
 v_sdk_platform=35
 v_sdk_build_tools=35.0.0
 
